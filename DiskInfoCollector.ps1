a4e59a9d238782c4bfaa3daefdc9cb0f6a73362a
HeavenElite <heaven.elite@gmail.com>
2022-09-25 21:11:17 +0800

    File Content Modification

--- DiskInfoCollector.ps1
+++ DiskInfoCollector.ps1
@@ -7,5 +7,3 @@ for ( $i = 0; $i -lt $Computers.Length; $i++) {
 
     $Result =  Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Get-PSDrive -PSProvider FileSystem} -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
     
-[System.Console]::Beep(1000,1000)
-
