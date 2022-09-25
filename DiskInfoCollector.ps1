5c783aa4e1b71cc0e6102bc3f45f58bbcaccac80
HeavenElite <heaven.elite@gmail.com>
2022-09-25 23:21:11 +0800

    Clear

--- DiskInfoCollector.ps1
+++
@@ -1,9 +0,0 @@
-$Computers = Import-Csv .\ITLabData.csv
-for ( $i = 0; $i -lt $Computers.Length; $i++) {
-
-    $Username   = $Computers[$i].Account
-    $Password   = ConvertTo-SecureString -AsPlainText $Computers[$i].Password -Force
-    $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
-
-    $Result =  Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Get-PSDrive -PSProvider FileSystem} -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-    
