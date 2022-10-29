17481cc07f00eaa1f1409bcc735ca07dc78b37a2
HeavenElite <heaven.elite@gmail.com>
2022-10-30 00:07:00 +0800

    Updated

--- Practise.py
+++
@@ -1,7 +0,0 @@
-countries = ['China', 'USA', 'JP', 'Russia']
-
-print(f'The nations here were {countries[0]}, {countries[1]} and {countries[2]}.\nBut {countries.pop(2)} is falling.')
-
-countries.append('France')
-
-print(f'So, Let us welcome {countries[3]}.')
