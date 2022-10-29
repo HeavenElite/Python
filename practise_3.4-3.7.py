2456dec82f8fe1631462dab2e1c3c61964752fe4
HeavenElite <heaven.elite@gmail.com>
2022-10-30 00:07:15 +0800

    Updated

---
+++ practise_3.4-3.7.py
@@ -0,0 +1,17 @@
+frute = ['Apple', 'Banana', 'Pear']
+countries = ['China', 'USA', 'JP', 'Russia',frute]
+
+print(f'The nations here were {countries[0]}, {countries[1]} and {countries[2]}.\nBut {countries.pop(2)} is falling.')
+
+countries.append('France')
+countries.insert(0,'Qing')
+
+print(f'So, Let us welcome {countries[3]}.\nWe are sorry that the following countries are removed from this meeting: {countries.pop(1)}, {countries.pop(1)} and {countries.pop(1)}.')
+
+countries.remove(frute)
+countries.pop()
+
+print(f'Now, the host nation of this meeting is {countries[0]}.')
+
+del countries[0]
+print(countries)
