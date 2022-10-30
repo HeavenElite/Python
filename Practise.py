7c8e983ada9349975e13a08c36147aab0d204739
HeavenElite <heaven.elite@gmail.com>
2022-10-30 22:41:34 +0800

    Updated

--- Practise.py
+++
@@ -1,6 +0,0 @@
-cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort(reverse=True)
-print(cars)
-
-print(sorted(cars,reverse=True)) 
-print(cars)
---
+++ practise_3.8.py
@@ -0,0 +1,30 @@
+travel_destinations = ['Russia', 'USA', 'Dubai', 'China', 'HongKong', 'Korea']
+
+print(sorted(travel_destinations))
+print(travel_destinations)
+print()
+
+print(sorted(travel_destinations, reverse=True))
+print(travel_destinations)
+print()
+
+travel_destinations.reverse()
+print(travel_destinations)
+print(travel_destinations)
+print()
+
+travel_destinations.reverse()
+print(travel_destinations)
+print(travel_destinations)
+print()
+
+travel_destinations.sort()
+print(travel_destinations)
+print(travel_destinations)
+print()
+
+travel_destinations.sort(reverse=True)
+print(travel_destinations)
+print(travel_destinations)
+print()
+
