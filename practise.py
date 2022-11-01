69520d85115f90d9cbe0cf9200c773b1b83935d6
HeavenElite <heaven.elite@gmail.com>
2022-11-01 23:46:01 +0800

    Updated

--- practise.py
+++ practise.py
@@ -1,10 +1,2 @@
-squares = []
-
-for count in range(1, 6, 1):
-    squares.insert(count - 1, count ** 3)
-
-print(squares)
-
-squares_2nd = [ value ** 3 for value in range(1, 6, 1) ]
-
-print(squares_2nd)
+numbers = [value for value in range(1,11)]
+print(numbers[5:11                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ])
