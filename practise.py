990e73e99faa1d07402559e0790fcee5913c50a9
HeavenElite <heaven.elite@gmail.com>
2022-11-01 23:20:33 +0800

    Updated

--- practise.py
+++ practise.py
@@ -1,14 +1,10 @@
 squares = []
-for value in range(1, 6):
-    square = value ** 3
-    squares.append(square)
 
-print(squares)
-
-for count in range(1, 6):
+for count in range(1, 6, 1):
     squares.insert(count - 1, count ** 3)
 
 print(squares)
-print(max(squares))
-print(min(squares))
-print(sum(squares))
+
+squares_2nd = [ value ** 3 for value in range(1, 6, 1) ]
+
+print(squares_2nd)
