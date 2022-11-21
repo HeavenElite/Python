61543b7fb9d0296d3aa7e6ae22e1b9f72ca336a9
HeavenElite <heaven.elite@gmail.com>
2022-11-21 13:35:00 +0800

    Updated

---
+++ practise.py
@@ -0,0 +1,6 @@
+numbers = []
+
+for number in range(1, 10, 2):
+    numbers.append(number)
+
+print(numbers)
--- practise_4.10_4.11.py
+++ practise_4.10_4.11.py
@@ -5,12 +5,6 @@ print(numbers)
 my_pizza = ['meet', 'frute', 'vegetable']
 friend_pizza = my_pizza[:]
 
-my_pizza.insert(0,'banana')
-friend_pizza.append('appple')
-
-print('My favorite pizzas are:')
-for pizza in my_pizza:
-    print(pizza)
 
 print("My friend's favorite pizzas are:")
 for pizza in friend_pizza:
