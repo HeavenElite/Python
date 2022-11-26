14e149f9acc464041535eddc0563717eb97a4d1a
HeavenElite <heaven.elite@gmail.com>
2022-11-26 16:46:14 +0800

    Updated

--- practise_6.6.py
+++ practise_6.6.py
@@ -1,14 +1,19 @@
 person = {
-    'given_name': '33',
+    'city': 's',
+    'given_name': 33,
     'surname': 'nie',
-    'age': '33',
-    'city': 'beijing',
+
 }
 
-properties = ['given_name', 'surname', 'age', 'city', 'country']
 
-for property in properties:
-    if person.get(property) != None:
-        print(f"The Key {property.title()} is existing.")
-    elif person.get(property) == None:
-        print(f"Please set a new value for this Key: {property.title()}")
+print(person.values())
+print(set(person.values()))
+
+
+#properties = ['given_name', 'surname', 'age', 'city', 'country']
+
+#for property in properties:
+#    if person.get(property) != None:
+#        print(f"The Key {property.title()} is existing.")
+#    elif person.get(property) == None:
+#        print(f"Please set a new value for this Key: {property.title()}")
