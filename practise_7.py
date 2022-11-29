a5339605f51eeda23c335806a4806906e6a9ab96
HeavenElite <heaven.elite@gmail.com>
2022-11-29 10:23:49 +0800

    Updated

--- practise_7.py
+++ practise_7.py
@@ -1,3 +1,5 @@
+# Practise 7-4
+
 prompt  = "\nPlease pick toppings for your pizza~"
 prompt += "\nInput 'Quit' to submit this order.\n"
 
@@ -20,3 +22,28 @@ print("All the toppings are:")
 for topping in toppings:
     print(f"{topping.title()}")
 print("\nWe are working on it.\n")
+
+# Practise 7-5
+
+prompt  = "\nWould you please tell me you age?"
+prompt += "\nPlease input Quit, after it's done.\n"
+
+active = True
+
+while active:
+    age = input(prompt)
+    if age.lower() != 'quit':
+
+        age = int(age)
+        if age < 3:
+            fee = 0
+        elif age <= 12:
+            fee = 10
+        elif age > 12:
+            fee = 15
+
+        print(f"Your ticket price is ${fee}.")
+    
+    else:
+        print("\nGood Bye~\n")
+        break
