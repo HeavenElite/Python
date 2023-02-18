dfa4a9c231ead799603f74a6d70f1f6b0a46b2cc
HeavenElite <heaven.elite@gmail.com>
2023-02-18 17:45:45 +0800

    Updated

--- function.py
+++ function.py
@@ -1,4 +1,6 @@
-def make_pizza(location, *toppings, service, nation='China', **customers):
+def make_pizza(
+        location, *toppings, service,
+        nation='China', **customers):
     """打印顾客点的所有配料"""
     print()
     print(f"User service level is: {service.title()}.")
@@ -9,10 +11,14 @@ def make_pizza(location, *toppings, service, nation='China', **customers):
         print(topping.title(), end=', ')
     print("\nUser name is ", end='')
     for first_name, last_name in customers.items():
-        print(f"{first_name.title()} {last_name.title()}, ", end='')
+        print(f"{first_name.title()} {last_name.title()}, ", 
+                end='')
     print("\n")
+
 
 def make_pizza(*args, **kwargs):
     """测试函数"""
     print(args)
     print(kwargs)
+
+
