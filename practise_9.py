f723309f0a5d90688ffc0d4f6b8510f0836378c8
HeavenElite <heaven.elite@gmail.com>
2023-03-04 22:18:39 +0800

    Updated

--- practise_9.py
+++ practise_9.py
@@ -1,3 +1,31 @@
+## 联系 3
+
+class User:
+    """练习 9-3"""
+
+    def __init__(self, **keyargs):
+        """初始化属性"""
+        self.names = {}
+        for first_name, last_name in keyargs.items():
+            self.names[first_name.title()] = last_name.title()
+
+    def describe_user(self):
+        """描述用户"""
+        print("User List:")
+        count = 1
+        for first_name, last_name in self.names.items():
+            print(f"{count}: {first_name} {last_name}")
+            count += 1
+    
+    def greet_user(self):
+        """欢迎用户"""
+        for first_name, last_name in self.names.items():
+            print(f"Hello, {first_name} {last_name}!")
+
+students_user = User(Jack='Brown', Laura='White')
+students_user.greet_user()   
+
+
 ## 练习 2
 
 class Restaurant:
